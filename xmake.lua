5574bf2ad9a5aa8b0593254e463ae1912ea759f0
MacroModel <MacroModel_Traject@outlook.com>
2025-03-20 12:16:51 +0800

    [profile] add files

---
+++ xmake.lua
@@ -0,0 +1,9 @@
+set_xmakever("2.8.2")
+
+set_project("uwvm")
+
+set_version("2.0.0")
+add_defines("UWVM_VERSION_X=2")
+add_defines("UWVM_VERSION_Y=0")
+add_defines("UWVM_VERSION_Z=0")
+add_defines("UWVM_VERSION_S=0")
