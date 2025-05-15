411c24f8b14f86493f5d3ccebb7c72a174b61048
MacroModel <MacroModel_Traject@outlook.com>
2025-05-15 22:54:15 +0800

    [utils] add unnecessary char8_t sz == std::byte sz checking

--- src/uwvm2/utils/cmdline/handle.h
+++ src/uwvm2/utils/cmdline/handle.h
@@ -474,6 +474,9 @@ UWVM_MODULE_EXPORT namespace uwvm2::utils::cmdline
         }
         else
         {
+            // Because the char8_t size is always equal to ::std::byte
+            static_assert(sizeof(char8_t) == sizeof(::std::byte));
+            
             auto const i{reinterpret_cast<::std::byte const*>(str.data())};
             crc32c.update(i, i + str.size());
         }
