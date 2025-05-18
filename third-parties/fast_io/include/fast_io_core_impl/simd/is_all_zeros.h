ca76eeb4faccfbe86374e106a4e547824a7915ab
MacroModel <MacroModel_Traject@outlook.com>
2025-05-18 15:34:07 +0800

    [fast_io] support __builtin_ia32_cmpb512_mask

--- third-parties/fast_io/include/fast_io_core_impl/simd/is_all_zeros.h
+++ third-parties/fast_io/include/fast_io_core_impl/simd/is_all_zeros.h
@@ -181,6 +181,13 @@ inline
 #else
 			return !__builtin_ia32_ptestmb512((x86_64_v64qi)vec.value, (x86_64_v64qi)vec.value, UINT_LEAST64_MAX);
 #endif
+#elif defined(__AVX512BW__) && defined(__x86_64__) && __has_builtin(__builtin_ia32_cmpb512_mask)
+			using x86_64_v64qi [[__gnu__::__vector_size__(64)]] = char;
+#if __has_builtin(__builtin_bit_cast)
+			return !__builtin_ia32_cmpb512_mask(__builtin_bit_cast(x86_64_v64qi, vec), x86_64_v64qi{}, 0x04, UINT_LEAST64_MAX);
+#else
+			return !__builtin_ia32_cmpb512_mask((x86_64_v64qi)vec.value, x86_64_v64qi{}, 0x04, UINT_LEAST64_MAX);
+#endif
 #endif
 #elif (defined(__x86_64__) || defined(_M_AMD64) || defined(__i386__) || defined(_M_IX86))
 			if constexpr (::fast_io::details::cpu_flags::avx512bw_supported)
