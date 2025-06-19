7cb059c20e197e4f069cfa7e761dafa93aa65f64
MacroModel <MacroModel_Traject@outlook.com>
2025-06-19 15:46:16 +0800

    [parser] Enhance static assertions for wasm_f32 and wasm_f64 to include precision checks

--- src/uwvm2/parser/wasm/standard/wasm1/type/value_type.h
+++ src/uwvm2/parser/wasm/standard/wasm1/type/value_type.h
@@ -107,7 +107,9 @@ UWVM_MODULE_EXPORT namespace uwvm2::parser::wasm::standard::wasm1::type
     using wasm_f32 = float;
 #endif
 
-    static_assert(::std::numeric_limits<wasm_f32>::is_iec559, "wasm_f32 ain't of the IEC 559/IEEE 754 floating-point types");
+    static_assert(::std::numeric_limits<wasm_f32>::is_iec559 && ::std::numeric_limits<wasm_f32>::digits == 24 && 
+                      ::std::numeric_limits<wasm_f32>::max_exponent == 128 && ::std::numeric_limits<wasm_f32>::min_exponent == -125, 
+                  "wasm_f32 ain't of the IEC 559/IEEE 754 floating-point types");
 
 #ifdef __STDCPP_FLOAT64_T__
     using wasm_f64 = ::std::float64_t;  // IEEE 754-2008
@@ -115,7 +117,9 @@ UWVM_MODULE_EXPORT namespace uwvm2::parser::wasm::standard::wasm1::type
     using wasm_f64 = double;
 #endif
 
-    static_assert(::std::numeric_limits<wasm_f64>::is_iec559, "wasm_f64 ain't of the IEC 559/IEEE 754 floating-point types");
+    static_assert(::std::numeric_limits<wasm_f64>::is_iec559 && ::std::numeric_limits<wasm_f64>::digits == 53 &&
+                      ::std::numeric_limits<wasm_f64>::max_exponent == 1024 && ::std::numeric_limits<wasm_f64>::min_exponent == -1021, 
+                  "wasm_f64 ain't of the IEC 559/IEEE 754 floating-point types");
 
     /// @brief      wasm base instruction, using a byte composition, instruction or extension instruction.
     /// @details    New feature
