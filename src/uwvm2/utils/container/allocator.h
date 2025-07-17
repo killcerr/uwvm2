f3f61d49538a51687e1ffbbe04095431088f6c17
YexuanXiao <bizwen@nykz.org>
2025-07-18 06:43:14 +0800

    [utils] fix fast_io_std_allocator

--- src/uwvm2/utils/container/allocator.h
+++ src/uwvm2/utils/container/allocator.h
@@ -42,62 +42,43 @@ import fast_io;
 UWVM_MODULE_EXPORT namespace uwvm2::utils::container
 {
 
-template <typename T>  
-struct fast_io_std_allocator 
-{  
-    using value_type = T;  
-    using pointer = T*;  
-    using const_pointer = const T*;  
-    using reference = T&;  
-    using const_reference = const T&;  
-    using size_type = ::std::size_t;  
-    using difference_type = ::std::ptrdiff_t;  
-      
-    using fast_io_type_allocator = ::fast_io::native_typed_global_allocator<T>;  
-      
-    inline constexpr fast_io_std_allocator() noexcept = default;  
-      
-    template<typename U>  
-    inline constexpr fast_io_std_allocator(const fast_io_std_allocator<U>&) noexcept {}  
-      
-    [[nodiscard]] inline constexpr T* allocate(::std::size_t n) noexcept
-    {  
-        return fast_io_type_allocator::allocate(n);  
-    }  
-      
-    [[nodiscard]] inline constexpr T* allocate(::std::size_t n, [[maybe_unused]] const void* hint) noexcept
-    {  
-        return fast_io_type_allocator::allocate(n);  
-    }  
-      
-    [[nodiscard]] inline constexpr ::std::pair<T*, ::std::size_t> allocate_at_least(::std::size_t n) noexcept
-    {  
-        auto const result{fast_io_type_allocator::allocate_at_least(n)};  
-        return {result.ptr, result.count};  
-    }  
-      
-    inline constexpr void deallocate(T* p, ::std::size_t n) noexcept 
-    {  
-        fast_io_type_allocator::deallocate_n(p, n);  
-    }  
-      
-    inline constexpr ::std::size_t max_size() const noexcept 
-    {  
-        return ::std::numeric_limits<::std::size_t>::max() / sizeof(T);  
-    }  
-    
-    template<typename U>  
-    inline constexpr bool operator==(const fast_io_std_allocator<U>&) const noexcept 
-    {  
-        return true; 
-    }  
-      
-    template<typename U>  
-    inline constexpr bool operator!=(const fast_io_std_allocator<U>&) const noexcept 
-    {  
-        return false;  
-    }  
-};  
-  
+    template <typename T>
+    struct fast_io_std_allocator
+    {
+        static_assert(::std::is_object_v<T> && !::std::is_const_v<T> && !std::is_volatile_v<T>);
+        using value_type = T;
+        using size_type = ::std::size_t;
+        using difference_type = ::std::ptrdiff_t;
+        using propagate_on_container_move_assignment = ::std::true_type;
+
+        using fast_io_type_allocator = ::fast_io::native_typed_global_allocator<T>;
+
+        inline constexpr fast_io_std_allocator() noexcept = default;
+
+        inline constexpr fast_io_std_allocator(fast_io_std_allocator const&) noexcept = default;
+
+        template <typename U>
+        inline constexpr fast_io_std_allocator(fast_io_std_allocator<U> const&) noexcept
+        {
+        }
+
+        [[nodiscard]] inline constexpr T* allocate(::std::size_t n) noexcept { return fast_io_type_allocator::allocate(n); }
+
+        [[nodiscard]] inline constexpr ::std::allocation_result<T*> allocate_at_least(::std::size_t n) noexcept
+        {
+            auto const [ptr, count]{fast_io_type_allocator::allocate_at_least(n)};
+            return {ptr, count};
+        }
+
+        inline constexpr void deallocate(T* p, ::std::size_t n) noexcept { fast_io_type_allocator::deallocate_n(p, n); }
+
+        inline constexpr ::std::size_t max_size() const noexcept { return ::std::numeric_limits<::std::size_t>::max() / sizeof(T); }
+
+        template <typename U, typename V>
+        friend inline constexpr bool operator== (fast_io_std_allocator<U> const&, fast_io_std_allocator<V> const&) noexcept
+        {
+            return true;
+        }
+    };
 
 }  // namespace uwvm2::utils::ansies
