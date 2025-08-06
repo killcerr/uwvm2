de495c23b059d58a035483755eb53a230cac6d37
MacroModel <liyn_xlfd@163.com>
2025-08-06 16:10:07 +0800

    [uwvm] Update command line parameter aliases for wasm preload, register, and main module name

--- src/uwvm2/uwvm/cmdline/params/wasm_register_dl.h
+++ src/uwvm2/uwvm/cmdline/params/wasm_register_dl.h
@@ -45,7 +45,7 @@ UWVM_MODULE_EXPORT namespace uwvm2::uwvm::cmdline::params
 
     namespace details
     {
-        inline constexpr ::uwvm2::utils::container::u8string_view wasm_register_dl_alias{u8"-Wrdl"};
+        inline constexpr ::uwvm2::utils::container::u8string_view wasm_register_dl_alias{u8"-Wdl"};
         extern "C++" ::uwvm2::utils::cmdline::parameter_return_type wasm_register_dl_callback(::uwvm2::utils::cmdline::parameter_parsing_results*,
                                                                                               ::uwvm2::utils::cmdline::parameter_parsing_results*,
                                                                                               ::uwvm2::utils::cmdline::parameter_parsing_results*) noexcept;
--- src/uwvm2/uwvm/cmdline/params/wasm_set_main_module_name.h
+++ src/uwvm2/uwvm/cmdline/params/wasm_set_main_module_name.h
@@ -42,7 +42,7 @@ UWVM_MODULE_EXPORT namespace uwvm2::uwvm::cmdline::params
     namespace details
     {
         inline bool wasm_set_main_module_name_is_exist{};
-        inline constexpr ::uwvm2::utils::container::u8string_view wasm_set_main_module_name_alias{u8"-Wsmmn"};
+        inline constexpr ::uwvm2::utils::container::u8string_view wasm_set_main_module_name_alias{u8"-Wname"};
         extern "C++" ::uwvm2::utils::cmdline::parameter_return_type
             wasm_set_main_module_name_callback(::uwvm2::utils::cmdline::parameter_parsing_results*,
                                                ::uwvm2::utils::cmdline::parameter_parsing_results*,
