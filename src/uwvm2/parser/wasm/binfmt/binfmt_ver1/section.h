cfca94657c714389bcba21eab24fa460add9f5eb
MacroModel <MacroModel_Traject@outlook.com>
2025-08-08 13:44:55 +0800

    [parser] fix macro

--- src/uwvm2/parser/wasm/binfmt/binfmt_ver1/section.h
+++ src/uwvm2/parser/wasm/binfmt/binfmt_ver1/section.h
@@ -132,7 +132,7 @@ UWVM_MODULE_EXPORT namespace uwvm2::parser::wasm::binfmt::ver1
         ::fast_io::operations::print_freestanding<false>(::std::forward<Stm>(stream), section_details(secs)...);
 #else
         /// @throws maybe throw fast_io::error, see the implementation of the stream
-        [&all_sections]<::std::size_t... I> UVM_ALWAYS_INLINE(::std::index_sequence<I...>, Stm&& stream) constexpr -> void
+        [&all_sections]<::std::size_t... I> UWVM_ALWAYS_INLINE(::std::index_sequence<I...>, Stm&& stream) constexpr -> void
         {
             ::fast_io::operations::print_freestanding<false>(::std::forward<Stm>(stream), section_details(get<I>(all_sections))...);
         }(::std::make_index_sequence<::std::tuple_size_v<::std::remove_cvref_t<decltype(all_sections)>>>{}, ::std::forward<Stm>(stream));
