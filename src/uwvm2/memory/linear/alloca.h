2700cf5cce764f11b91a3ea8115fafe9ba6f8323
MacroModel <liyn_xlfd@163.com>
2025-08-28 17:51:17 +0800

    [memory] Update custom page size handling to validate power of 2 and compute log2 without locking

--- src/uwvm2/memory/linear/alloca.h
+++ src/uwvm2/memory/linear/alloca.h
@@ -111,7 +111,13 @@ UWVM_MODULE_EXPORT namespace uwvm2::memory::linear
             this->growing_flag_p = atomic_flag_allcator_t::allocate(1uz);
             ::new(this->growing_flag_p)::std::atomic_flag{ATOMIC_FLAG_INIT};
 
-            set_custom_page_size(custom_page_size);
+            // The same method as set_custom_page_size, but without adding a lock.
+
+            // Check if it is a power of 2
+            if(static_cast<unsigned>(::std::popcount(custom_page_size)) != 1u) [[unlikely]] { ::fast_io::fast_terminate(); }
+
+            // Since there is only one pop, directly counting the number of zeros suffices to calculate log2
+            this->custom_page_size_log2 = ::std::countr_zero(custom_page_size);
         }
 
         inline constexpr void set_custom_page_size(::std::size_t custom_page_size) noexcept
