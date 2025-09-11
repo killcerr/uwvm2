bc16d0e610062ffd46b47aaa0755ce4b84efac77
MacroModel <MacroModel_Traject@outlook.com>
2025-09-11 09:06:05 +0800

    [test] mv wasip1 func

---
+++ test/0008.imported/wasi/wasip1/platform/get_env_from_platform.cc
@@ -0,0 +1,36 @@
+﻿/*************************************************************
+ * Ultimate WebAssembly Virtual Machine (Version 2)          *
+ * Copyright (c) 2025-present UlteSoft. All rights reserved. *
+ * Licensed under the APL-2.0 License (see LICENSE file).    *
+ *************************************************************/
+
+/**
+ * @author      GPT-5
+ * @version     2.0.0
+ * @copyright   APL-2.0 License
+ */
+
+/****************************************
+ *  _   _ __        ____     __ __  __  *
+ * | | | |\ \      / /\ \   / /|  \/  | *
+ * | | | | \ \ /\ / /  \ \ / / | |\/| | *
+ * | |_| |  \ V  V /    \ V /  | |  | | *
+ *  \___/    \_/\_/      \_/   |_|  |_| *
+ *                                      *
+ ****************************************/
+
+#include <cstddef>
+#include <cstdint>
+
+#include <uwvm2/imported/wasi/wasip1/platform/get_process_env.h>
+
+int main()
+{
+    ::std::size_t idx{};
+    for(auto const& env: ::uwvm2::imported::wasi::wasip1::platform::get_process_env())
+    {
+        ::fast_io::println(::fast_io::u8out(), idx++, u8": ", env);
+    }
+    return 0;
+}
+
